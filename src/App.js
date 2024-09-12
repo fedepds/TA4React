3809cf97169fc6ca27bb401334a18bb236f2f640
Federico Pizarro <127800142+fedepds@users.noreply.github.com>
2024-09-12 10:20:52 -0300

    Empezar TA4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+let Button =()=>{
+  <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header>
+    const [count, setCount] = useState(0);
+  
+    function handleClick() {
+      setCount(count + 1);
+    }
+  
+    return (
+      <button onClick={handleClick}>
+        Hiciste clic {count} veces
+      </button>
+    );
+  
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button></Button>
+
     </div>
   );
 }
